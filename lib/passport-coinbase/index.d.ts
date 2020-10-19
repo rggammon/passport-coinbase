3705f44f441b194d4cd6af06eeb06907c7054de1
Ryan Gammon <ryan@gamnation.net>
2020-10-18 17:44:04 -0700

    Add type definitions

---
+++ lib/passport-coinbase/index.d.ts
@@ -0,0 +1,37 @@
+declare type OAuth2StrategyOptions = import("passport-oauth2").StrategyOptions;
+declare type PassportProfile = import("passport").Profile
+
+declare module "passport-coinbase" {
+    export interface Profile extends PassportProfile {
+        id: string;
+        displayName: string;
+        email: string;
+
+        _raw: string;
+        _json: any;
+    }
+
+    export interface StrategyOptions extends OAuth2StrategyOptions {
+        enableProof?: boolean;
+        profileFields?: string[];
+
+        authorizationURL?: string;
+        tokenURL?: string;
+        profileURL?: string;
+        graphAPIVersion?: string;
+
+        display?: 'page' | 'popup' | 'touch';
+
+        authType?: 'reauthenticate';
+        authNonce?: string;
+    }
+
+    export type VerifyFunction =
+        (accessToken: string, refreshToken: string, profile: Profile, done: (error: any, user?: any, info?: any) => void) => void;
+
+    export class Strategy implements passport.Strategy {
+        constructor(options: StrategyOptions, verify: VerifyFunction);
+        name: string;
+        authenticate(req: express.Request, options?: object): void;
+    }
+}
